67d84f18a669e8df7fbb332d1749cbfc8f6774a5
Andreas Schaeffer <andreas@schaeffernet.de>
2025-05-17 22:43:07 +0200

    Added pages for graphql schema visualization and graphql schema listing

---
+++ view/graphql/load-schema.js
@@ -0,0 +1,11 @@
+async function loadSchema(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    document.getElementById("sdl").innerHTML = await response.text();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
